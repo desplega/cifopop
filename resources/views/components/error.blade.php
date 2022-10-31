eff3c388052ee996e44473ca6049be533686f61d
desplega <desplega@desplega.com>
2022-10-31 18:28:56 +0100

    Added personlised error pages for 403, 404 and 419

---
+++ resources/views/components/error.blade.php
@@ -0,0 +1,9 @@
+<div class="p-5 text-center">
+    <img class="m-auto h-60" src="{{ $image }}" alt="Error" title="Error">
+    <div class="mt-4 block text-xl font-bold">
+        {{ $error }}
+    </div>
+    <div class="mt-4 text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+        {{ $description }}
+    </div>
+</div>
---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Ooops! Something went wrong!') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {{-- Content --}}
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-4">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <x-error
+                        error="{{ __('Forbidden') }}"
+                        image="{{ asset('images/errors/error-403.png') }}"
+                        description="{{ __('You don\'t have rights to do this operation.') }}"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
